351b975b474cd9ca80f7575ed976c7014b304104
Francesco Anselmo <francesco.anselmo@gmail.com>
2024-05-19 20:18:42 +0100

    filtering by point, improved format for output

--- bos-testing/gcp-pubsub-receive/gcp-pubsub-receive.py
+++ bos-testing/gcp-pubsub-receive/gcp-pubsub-receive.py
@@ -20,57 +20,76 @@ __status__ = "Dev"
 from concurrent.futures import TimeoutError
 from google.cloud import pubsub_v1
 import argparse
+import json
+from tabulate import tabulate
 from pyfiglet import *
 
 TARGET_DEVICE_ID = ""
 TARGET_GATEWAY_ID = ""
 TARGET_SUBFOLDER = ""
 TARGET_TYPE = ""
+TARGET_POINT_NAME = ""
 
-def print_message(message):
-  print(f"\nReceived {message}.")
+def print_message(message, pointname):
   body = message.data
-  print(body)
+  device_id = message.attributes['deviceId']
+  gateway_id = message.attributes['gatewayId']
+  sub_folder = message.attributes['subFolder']
+  type = message.attributes['subType']
+  timestamp = json.loads(body)['timestamp']
   
+  if pointname != "":
+    pointset = json.loads(body)['points']
+    try:
+      filtered_data = {key: pointset[key] for key in [pointname]}
+      point_value = filtered_data[pointname]['present_value']
+      print(tabulate([["Timestamp", "Device ID", "Gateway ID", "Subfolder", "Type", "Point name", "Point value"],
+                      [timestamp, device_id, gateway_id, sub_folder, type, pointname, point_value]]))
+    except:
+      pass
+  else:
+    print(tabulate([["Timestamp", "Device ID", "Gateway ID", "Subfolder", "Type"],
+                      [timestamp, device_id, gateway_id, sub_folder, type]]))
+    print(message)
+    print(body)
+    print(70*"-")
+    
 
 def message_callback(message: pubsub_v1.subscriber.message.Message) -> None:
-    global TARGET_DEVICE_ID, TARGET_GATEWAY_ID, TARGET_SUBFOLDER, TARGET_TYPE
-    
-    # print_message(message)
+    global TARGET_DEVICE_ID, TARGET_GATEWAY_ID, TARGET_SUBFOLDER, TARGET_TYPE, TARGET_POINT_NAME
 
     device_id = message.attributes['deviceId']
     gateway_id = message.attributes['gatewayId']
     sub_folder = message.attributes['subFolder']
     type = message.attributes['subType']
     
-    # print(device_id, gateway_id, sub_folder, type)
     
     if TARGET_DEVICE_ID in device_id:
       if gateway_id in TARGET_GATEWAY_ID:
         if sub_folder == TARGET_SUBFOLDER and TARGET_SUBFOLDER != "":
           if type == TARGET_TYPE:
-            print_message(message)
+            print_message(message, TARGET_POINT_NAME)
         elif TARGET_SUBFOLDER == "" and TARGET_TYPE == "":
-          print_message(message)
+          print_message(message, TARGET_POINT_NAME)
       elif TARGET_GATEWAY_ID == "":
         if sub_folder == TARGET_SUBFOLDER and TARGET_SUBFOLDER != "":
           if type == TARGET_TYPE:
-            print_message(message)
+            print_message(message, TARGET_POINT_NAME)
         elif TARGET_SUBFOLDER == "" and TARGET_TYPE == "":
-          print_message(message)
+          print_message(message, TARGET_POINT_NAME)
     elif TARGET_DEVICE_ID == "":
       if gateway_id in TARGET_GATEWAY_ID:
         if sub_folder == TARGET_SUBFOLDER and TARGET_SUBFOLDER != "":
           if type == TARGET_TYPE:
-            print_message(message)
+            print_message(message, TARGET_POINT_NAME)
         elif TARGET_SUBFOLDER == "" and TARGET_TYPE == "":
-          print_message(message)
+          print_message(message, TARGET_POINT_NAME)
       elif TARGET_GATEWAY_ID == "":
         if sub_folder == TARGET_SUBFOLDER and TARGET_SUBFOLDER != "":
           if type == TARGET_TYPE:
-            print_message(message)
+            print_message(message, TARGET_POINT_NAME)
         elif TARGET_SUBFOLDER == "" and TARGET_TYPE == "":
-          print_message(message)
+          print_message(message, TARGET_POINT_NAME)
     message.ack()
 
 
@@ -82,7 +101,7 @@ def show_title():
   print(f1.renderText('receive'))
 
 def main():
-  global TARGET_DEVICE_ID, TARGET_GATEWAY_ID, TARGET_SUBFOLDER, TARGET_TYPE
+  global TARGET_DEVICE_ID, TARGET_GATEWAY_ID, TARGET_SUBFOLDER, TARGET_TYPE, TARGET_POINT_NAME
       
   show_title()
 
@@ -95,6 +114,7 @@ def main():
   parser.add_argument("-g", "--gateway", default="", help="filter for the gatewayId attribute (optional)")
   parser.add_argument("-f", "--folder", default="", help="filter for the subFolder attribute (optional, if not specified shows messages in all folders)")
   parser.add_argument("-y", "--type", default="", help="filter for the subType attribute (optional)")
+  parser.add_argument("-n", "--pointname", default="", help="filter for the point name (optional)")
   parser.add_argument("-t", "--timeout", default="60", help="time interval in seconds for which to receive messages (optional, default=60 seconds)")
 
   args = parser.parse_args()
@@ -111,6 +131,7 @@ def main():
     TARGET_GATEWAY_ID = args.gateway
     TARGET_SUBFOLDER = args.folder
     TARGET_TYPE = args.type
+    TARGET_POINT_NAME = args.pointname
 
     # Number of seconds the subscriber should listen for messages
     TIMEOUT = int(args.timeout)
